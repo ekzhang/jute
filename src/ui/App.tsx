21c93d24d1491581645cb1661114c74b0c2dfb44
Eric Zhang <ekzhang1@gmail.com>
2024-02-11 23:37:39 -0500

    Tweak horizontal spacing of sidebar

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -6,20 +6,23 @@ export default () => {
   return (
     <main className="absolute inset-0 bg-white">
       {/* Title bar */}
-      <div className="absolute inset-x-0 z-10 bg-gradient-to-b from-white/75 from-60% to-white/0 pb-5 pt-1 text-center text-sm">
-        {title}
+      <div
+        className="absolute inset-x-0 z-10 bg-gradient-to-b from-white/75 from-60% to-white/0 pb-3 pt-1 text-center text-sm"
+        data-tauri-drag-region
+      >
+        <span className="pointer-events-none">{title}</span>
       </div>
 
       {/* Main notebook */}
-      <div className="grid h-full overflow-y-auto sm:grid-cols-[minmax(0,3fr),minmax(0,1fr)]">
-        <div className="max-w-screen-md px-4 py-16">
+      <div className="grid h-full overflow-y-auto md:grid-cols-[600px,minmax(0,1fr)] lg:grid-cols-[800px,minmax(0,1fr)]">
+        <div className="px-4 py-16">
           <NotebookView />
         </div>
         <div
-          className="hidden border-l border-zinc-200 bg-zinc-100 sm:block"
+          className="hidden border-l border-zinc-200 bg-zinc-100 md:block"
           data-tauri-drag-region
         >
-          <p>test</p>
+          <p></p>
         </div>
       </div>
     </main>
