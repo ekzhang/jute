3ce8f748d7305701951b97bd3cf386d573b21fd9
Eric Zhang <ekzhang1@gmail.com>
2024-04-20 14:38:32 -0400

    CornerDownRightIcon

--- src/ui/NotebookCells.tsx
+++ src/ui/NotebookCells.tsx
@@ -1,4 +1,4 @@
-import { CircleIcon } from "lucide-react";
+import { CircleIcon, CornerDownRightIcon } from "lucide-react";
 import { useContext } from "react";
 
 import CmEditor from "./CmEditor";
@@ -14,15 +14,20 @@ export default () => {
     <div className="py-16">
       {editorIds.map((id) => (
         <div key={id}>
-          <div className="flex items-start gap-2">
-            <CircleIcon className="my-[6px] h-3.5 w-3.5 fill-zinc-300 stroke-none" />
+          <div className="flex items-start gap-2 px-4">
+            <CircleIcon className="my-[6px] h-3.5 w-3.5 fill-zinc-200 stroke-none" />
             <div className="flex-1">
               <CmEditor editorId={id} />
             </div>
           </div>
           {notebook.outputs.get(id) && (
-            <div className="px-6 pt-4">
-              <OutputView value={notebook.outputs.get(id)} />
+            <div className="flex gap-3 px-4 pt-4">
+              <div>
+                <CornerDownRightIcon className="ml-1 h-5 w-5 text-green-700" />
+              </div>
+              <div className="pt-0.5">
+                <OutputView value={notebook.outputs.get(id)} />
+              </div>
             </div>
           )}
           <hr className="mx-2 my-4 border-zinc-200" />
