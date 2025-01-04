ead498f37b72443ed07e95e6a06eedf3186e5960
Julian LaNeve <lanevejulian@gmail.com>
2025-01-04 13:41:27 -0500

    Show kernel status (currently yellow/red) when opening a notebook

--- src/pages/NotebookPage.tsx
+++ src/pages/NotebookPage.tsx
@@ -1,17 +1,40 @@
+import { useEffect, useMemo } from "react";
 import { useSearch } from "wouter";
 
+import { Notebook, NotebookContext } from "@/stores/notebook";
 import NotebookFooter from "@/ui/notebook/NotebookFooter";
 import NotebookHeader from "@/ui/notebook/NotebookHeader";
 import NotebookView from "@/ui/notebook/NotebookView";
 
 export default function NotebookPage() {
   const { path } = Object.fromEntries(new URLSearchParams(useSearch()));
+  // Single mutable object that is shared between all parts of the notebook.
+  const notebook = useMemo(() => new Notebook(path), [path]);
+
+  useEffect(() => {
+    notebook.addCell(`print("Hello, world!")`);
+    notebook.addCell(`for i in range(100):
+    if i % 15 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)`);
+    notebook.addCell(`import matplotlib.pyplot as plt
+import numpy as np
+
+plt.plot(np.random.normal(size=(400,)).cumsum())`);
+  }, [notebook]);
 
   return (
     <main className="absolute inset-0 bg-white">
-      <NotebookHeader kernelName="Local Kernel (Python 3.11.7)" />
-      <NotebookView path={path} />
-      <NotebookFooter />
+      <NotebookContext.Provider value={notebook}>
+        <NotebookHeader kernelName="Local Kernel (Python 3.11.7)" />
+        <NotebookView />
+        <NotebookFooter />
+      </NotebookContext.Provider>
     </main>
   );
 }
--- src/ui/notebook/NotebookHeader.tsx
+++ src/ui/notebook/NotebookHeader.tsx
@@ -1,5 +1,7 @@
+import clsx from "clsx";
 import {
   ChartLineIcon,
+  DotIcon,
   HomeIcon,
   PlayIcon,
   PlusIcon,
@@ -7,12 +9,19 @@ import {
   SettingsIcon,
 } from "lucide-react";
 import { Link } from "wouter";
+import { useStore } from "zustand";
+
+import { useNotebook } from "@/stores/notebook";
 
 type Props = {
   kernelName: string;
 };
 
 export default function NotebookHeader({ kernelName }: Props) {
+  const notebook = useNotebook();
+
+  const kernelId = useStore(notebook.store, (state) => state.kernelId);
+
   return (
     <div className="absolute inset-x-0 h-16 bg-gradient-to-b from-white/85 from-40% to-white/0">
       <header
@@ -31,8 +40,16 @@ export default function NotebookHeader({ kernelName }: Props) {
             <RefreshCwIcon size={16} />
           </button>
 
-          <button className="mx-2 w-60 rounded border border-gray-200 py-[3px] text-xs text-gray-900 transition-all hover:border-gray-400 hover:bg-gray-100 active:scale-105">
+          <button className="mx-2 flex w-60 items-center justify-center rounded border border-gray-200 py-[3px] text-xs text-gray-900 transition-all hover:border-gray-400 hover:bg-gray-100 active:scale-105">
+            <DotIcon
+              className={clsx(
+                "w-6",
+                kernelId === "" ? "text-orange-500" : "text-green-500",
+              )}
+            />
             {kernelName}
+            <div className="w-6" />{" "}
+            {/* Same width as the icon to make sure kernel name is center aligned */}
           </button>
 
           <button className="rounded p-1 text-gray-500 transition-all hover:bg-gray-100 hover:text-black active:scale-110">
