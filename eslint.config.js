8df16a9232643d6abe20121b3174b900622079c0
Julian LaNeve <lanevejulian@gmail.com>
2025-01-05 01:07:46 -0500

    remove bindings from eslint ignore

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist", "src-tauri", "src/bindings"] },
+  { ignores: ["dist", "src-tauri"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
