f6fcf5df81960b6d79d0fe40f50dab1c7bb26a41
Eric Zhang <ekzhang1@gmail.com>
2025-01-08 15:41:42 -0500

    Update main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -13,9 +13,9 @@ use jute::{
     Error,
 };
 use sysinfo::System;
-use tauri::{ipc::Channel, AppHandle};
 #[allow(unused_imports)]
 use tauri::Manager;
+use tauri::{ipc::Channel, AppHandle};
 use tracing::info;
 
 #[tauri::command]
