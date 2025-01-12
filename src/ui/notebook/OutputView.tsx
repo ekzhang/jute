b1c88a4575d07ecf59ccdcd6188ad90c64c08090
Eric Zhang <ekzhang1@gmail.com>
2025-01-12 18:37:46 -0500

    Allow copying and selecting the outputs of cells

--- src/ui/notebook/OutputView.tsx
+++ src/ui/notebook/OutputView.tsx
@@ -11,13 +11,8 @@ export default function OutputView({ value }: Props) {
     return null;
   }
   return (
-    <div className="px-8 pb-6 pt-4 after:contents">
-      <pre
-        className={clsx(
-          value.status === "error" && "text-red-500",
-          "select-text whitespace-pre-wrap break-words text-sm",
-        )}
-      >
+    <div className="select-text whitespace-pre-wrap break-words px-8 pb-6 pt-4 text-sm after:contents">
+      <pre className={clsx(value.status === "error" && "text-red-500")}>
         {value.output}
       </pre>
 
